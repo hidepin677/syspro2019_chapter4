db4f910a34f19e84dd7a873b04f829a49ce0b68a
hidepin677 <3jtanizaki@gmail.com>
2019-09-26 03:13:20 +0100

    追加

--- servo.py
+++ servo.py
@@ -1,5 +1,7 @@
 import time
 import RPi.GPIO as GPIO
+def setservo(deg):
+   servo.ChangeDutyCycle(2.5 + 9.5 * ( deg + 90) / 180 )
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(2, GPIO.OUT)
@@ -12,12 +14,15 @@ top = 12.0
 
 for i in range(5):
 	servo.ChangeDutyCycle(bottom)
+        setservo(-90)
 	time.sleep(1.0)
 
 	servo.ChangeDutyCycle(middle)
+        setservo(0)
 	time.sleep(1.0)
 
 	servo.ChangeDutyCycle(top)
+        setservo(90)
 	time.sleep(1.0)
 
 
